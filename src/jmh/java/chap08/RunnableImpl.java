e994a8f8525a2f70b5749c22554283127f175108
wisehero10 <godjw96@gmail.com>
2023-07-29 16:17:55 +0900

    Thread 클래스와 Runnable 인터페이스

---
+++ src/jmh/java/chap08/RunnableImpl.java
@@ -0,0 +1,8 @@
+package chap08;
+
+public class RunnableImpl implements Runnable{
+	@Override
+	public void run() {
+		System.out.println("This is RunnableImpl");
+	}
+}
---
+++ src/jmh/java/chap08/ThreadExtends.java
@@ -0,0 +1,8 @@
+package chap08;
+
+public class ThreadExtends extends Thread {
+	@Override
+	public void run() {
+		System.out.println("This is ThreadExtends");
+	}
+}
